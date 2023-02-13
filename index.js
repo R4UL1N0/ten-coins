1a1ebae167fb8e2f27148635db4fb4a585acc8b1
unknown <h6sw97@gmail.com>
2023-02-12 22:35:19 -0300

    moving all the project to github // not the best practices everfound

---
+++ index.js
@@ -0,0 +1,96 @@
+import express from 'express'
+import axios from 'axios'
+import jsdom from 'jsdom'
+import { CoinModel } from './models/CoinModel.js'
+
+const app = express()
+
+const { JSDOM } = jsdom
+
+const url = 'https://coinmarketcap.com/'
+
+const PORT = process.env.PORT || 3000
+
+async function getInfoCoinMarkerCap() {
+
+    const req = await axios.get(url)
+
+    const listObjs = []
+
+    if (req.status == 200) {
+
+        const dom = new JSDOM(req.data)
+
+        dom.window.document.querySelectorAll('tbody tr').forEach((el, i) => {
+            
+            if (i <= 9) {
+                const coinImage = el.querySelector('.coin-logo').src
+                const coinParamList = []
+                
+                
+                console.log(`--------COIN ${i + 1}----------`)
+                el.querySelectorAll('td').forEach((el, i) => {
+
+                    
+                    let content = el.textContent
+                    
+                    if (content != '' && i < 4) {
+                        if (i == 2) {
+                            const contentLength = content.length
+                            let name, sign
+
+                            for (var i = 0; i < contentLength - 1; i++) {
+                                if (parseInt(content.charAt(i))) {
+                                    name = content.slice(0, i)
+                                    sign = content.slice(i + 1)
+                                    coinParamList.push(name)
+                                    coinParamList.push(sign)
+                                }
+                            }
+                        } else {
+                            coinParamList.push(content)
+                        }
+                        
+                        
+                    }
+                })
+                coinParamList.push(coinImage)
+                console.log(coinParamList)
+                const coinObj = new CoinModel(coinParamList[0], coinParamList[1], coinParamList[2], coinParamList[3], coinParamList[4])
+                listObjs.push(coinObj)
+            }
+
+            
+
+            
+        })
+
+        return listObjs
+    }
+
+
+    
+}
+
+
+app.get('/', async (req, res) =>  {
+    res.json('Welcome to the mini crypto API')
+})
+
+app.get('/coins', async (req, res) =>  {
+    getInfoCoinMarkerCap().then((value) => {
+        res.json(value)
+    })
+})
+
+app.get('/coins/:id', (req, res) => {
+    const id = req.params.id
+    getInfoCoinMarkerCap().then((value) => {
+        res.json(value[id])
+    })
+
+})
+
+app.listen(PORT, () => {
+    console.log('RUNNING')
+})
---
+++ models/CoinModel.js
@@ -0,0 +1,9 @@
+export class CoinModel {
+    constructor(id, name, sign, price, image) {
+        this.id = id
+        this.name = name
+        this.sign = sign
+        this.price = price
+        this.image = image
+    }
+}
